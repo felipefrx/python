f1c89b939b535b1ddf0adb4ff105653b4b7b13bc
Felipe Ferreira <78969308+felipefrx@users.noreply.github.com>
2022-02-07 15:34:23 -0300

    Add files via upload
    Input Function.

---
+++ input_function.py
@@ -0,0 +1,12 @@
+import os #This module provides a portable way of using operating system dependent functionality.
+
+os.system('clear') #Linus #This command clean the console
+#os.system('cls') #Windows
+
+name = str(input("Type your name: ")) #The function input stores what the user has typed 
+age = int(input("Type your age: "))
+
+if age >= 18:
+    print("Wellcome, {}!".format(name))
+else:
+    print("Sorry, you don't have age for acess this program.")
