bbdff6f97a895cea53e0cd7d3ce24c46047bc504
Felipe Ferreira <78969308+felipefrx@users.noreply.github.com>
2022-02-07 14:46:27 -0300

    Add files via upload
    Loop for.

---
+++ loop_for.py
@@ -0,0 +1,16 @@
+names = ["Felipe", "Luisa", "Cassio", "Brenda"]
+
+for x in names:
+    print(x)
+    if x == "Felipe":
+        print("Man")
+    if x == "Cassio":
+        print("Man")
+    else:
+        print("Woman")
+print(" ")
+for x in ["Felipe", "Luisa", "Cassio", "Brenda"]:
+    print(x)
+    if (x == "Cassio"):
+        break
+print("Finished!")
