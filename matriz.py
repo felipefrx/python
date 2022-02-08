6ed617a8c9b7ab44044e0370f34d307f9be57b45
Felipe Ferreira <78969308+felipefrx@users.noreply.github.com>
2022-02-08 11:21:26 -0300

    Update matriz.py

--- matriz.py
+++ matriz.py
@@ -1,11 +1,11 @@
 carros = [
-    ["Modelo", "HRV"],
-    ["Fabricante", "Honda"],
-    ["Ano", 2016],
+    ["Model", "HRV"],
+    ["Manofacturer", "Honda"],
+    ["Year", 2016],
 ]
 
 carros[2][1] = 2019 #Changed value
-carros.append(["Cor", "Preto"]) #Add value
+carros.append(["Color", "Black"]) #Add value
 
 for x, y in carros:
     print(x,"|", y)
